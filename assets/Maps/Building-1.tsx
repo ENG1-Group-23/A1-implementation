348cdc7b16c4b0f61d7af653f5596074b0d0e9dc
MerlinG2 <132226686+MerlinG2@users.noreply.github.com>
2024-03-15 03:25:41 +0000

    Testing Buildings
    Added a few demo buildings but there is a rendering issue

---
+++ assets/Maps/Building-1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Building-1" tilewidth="96" tileheight="80" tilecount="1" columns="1">
+ <image source="Building-1.png" width="96" height="80"/>
+</tileset>

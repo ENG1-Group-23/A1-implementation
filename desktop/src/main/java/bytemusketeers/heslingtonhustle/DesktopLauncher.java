9c2ee7d92b11652715d5d7231852efda14ff5d62
Oliver Dixon <od641@york.ac.uk>
2024-03-15 18:07:56 +0000

    Do not limit FPS by the monitor refresh rate

--- desktop/src/main/java/bytemusketeers/heslingtonhustle/DesktopLauncher.java
+++ desktop/src/main/java/bytemusketeers/heslingtonhustle/DesktopLauncher.java
@@ -8,8 +8,33 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
  * instance, specifying certain fixed application configuration parameters.
  */
 public class DesktopLauncher {
+    /**
+     * The initial window height, in pixels
+     */
     private static final int INITIAL_WINDOW_WIDTH = 800;
+
+    /**
+     * The initial window width, in pixels
+     */
     private static final int INITIAL_WINDOW_HEIGHT = 480;
+
+    /**
+     * The frames-per-second limit, fixed to ensure a consistent {@link Character} move-speed across differing monitor
+     * refresh rates.
+     */
+    private static final int FPS_LIMIT = 60;
+
+    /**
+     * The title of the Java window
+     */
+    private static final String GAME_TITLE = "Heslington Hustle";
+
+    /**
+     * The file-path of the icon to be displayed on non-MacOS systems
+     *
+     * @see Lwjgl3ApplicationConfiguration#setWindowIcon(String...)
+     */
+    private static final String ICON_PATH = "logo.png";
 
     /**
      * Parameterises a {@link Lwjgl3ApplicationConfiguration} instance with sensible pre-determined defaults to be used
@@ -18,16 +43,13 @@ public class DesktopLauncher {
      * @return The parameterised default configuration for the {@link Lwjgl3Application} window
      */
     private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
-        final String WINDOW_TITLE = "Heslington Hustle";
-        final String WINDOW_ICON_PATH = "logo.png";
-
         Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
 
-        configuration.setTitle(WINDOW_TITLE);
+        configuration.setTitle(GAME_TITLE);
         configuration.useVsync(true);
-        configuration.setForegroundFPS(Lwjgl3ApplicationConfiguration.getDisplayMode().refreshRate);
+        configuration.setForegroundFPS(FPS_LIMIT);
         configuration.setWindowedMode(INITIAL_WINDOW_WIDTH, INITIAL_WINDOW_HEIGHT);
-        configuration.setWindowIcon(WINDOW_ICON_PATH);
+        configuration.setWindowIcon(ICON_PATH);
 
         return configuration;
     }

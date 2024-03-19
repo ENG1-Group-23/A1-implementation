86d3f1a338b353a49dcde83dc11ee8f4ff4d1ee7
AlbaraSh <albarashoukri1234@gmail.com>
2024-03-19 17:18:08 +0000

    minor change

--- core/src/main/java/bytemusketeers/heslingtonhustle/Screens/TitleScreen.java
+++ core/src/main/java/bytemusketeers/heslingtonhustle/Screens/TitleScreen.java
@@ -29,7 +29,7 @@ public class TitleScreen extends ScreenAdapter{
 
     @Override
     public void render(float delta) {
-        //shows text on the screen
+        //show text on the screen for instructions
         Gdx.gl.glClearColor(0, .25f, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         game.batch.begin();
